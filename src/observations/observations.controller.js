5268d9c0be397dedd2a02bbdb65f146d373994e2
Brett <brett_neighbors@yahoo.com>
2024-03-20 19:41:23 -0600

    Update observations controller with create and list

--- src/observations/observations.controller.js
+++ src/observations/observations.controller.js
@@ -1,4 +1,5 @@
 let nextId = 1;
+const observations = []
 
 const validSkyConditions = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109]
 
@@ -41,11 +42,20 @@ async function create(req, res) {
   newObservation.created_at = now;
   newObservation.updated_at = now;
 
+  observations.push(newObservation)
+
   res.status(201).json({
     data: newObservation,
   });
 }
 
+async function list(req, res) {
+    res.json({
+        data: observations,
+    });
+}
+
 module.exports = {
   create: [hasData, hasLatitude, hasLongitude, hasSkyCondition, create],
+  list
 };
